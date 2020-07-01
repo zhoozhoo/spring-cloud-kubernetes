42b48788c6a0950610d63b73e31b0a27cdc635fd
Zhubin Salehi <zhoozhoo@yahoo.com>
2020-07-01 10:59:37 -0400

    Initial commit

---
+++ admin-server/src/test/java/ca/zhoozhoo/springcloud/admin/AdminServerApplicationTests.java
@@ -0,0 +1,12 @@
+package ca.zhoozhoo.springcloud.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
