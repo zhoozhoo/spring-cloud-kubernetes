fa58172f7fa0800c0c3046463f8351e5ea5bc8ba
Zhubin Salehi <zhoozhoo@yahoo.com>
2020-08-04 12:40:55 -0400

    Added @EnableScheduling annotation for k8s

--- spring-cloud-kubernetes-admin-server/src/main/java/ca/zhoozhoo/spring/cloud/kubernetes/admin/AdminServerApplication.java
+++ spring-cloud-kubernetes-admin-server/src/main/java/ca/zhoozhoo/spring/cloud/kubernetes/admin/AdminServerApplication.java
@@ -3,11 +3,13 @@ package ca.zhoozhoo.spring.cloud.kubernetes.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @EnableAdminServer
 @EnableDiscoveryClient
+@EnableScheduling
 @SpringBootApplication
 public class AdminServerApplication {
 
